9d9352ce37ee462d5cada19734fb8db89aa2002d
Tomasz Posłuszny <tefposluszny@gmail.com>
2019-08-23 23:25:39 +0200

    Add mongoTemplate bean

--- src/main/java/com/devpeer/calories/MongoConfiguration.java
+++ src/main/java/com/devpeer/calories/MongoConfiguration.java
@@ -2,7 +2,11 @@ package com.devpeer.calories;
 
 
 import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
@@ -16,5 +20,10 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
     @Override
     public MongoClient mongoClient() {
         return new MongoClient();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
     }
 }
