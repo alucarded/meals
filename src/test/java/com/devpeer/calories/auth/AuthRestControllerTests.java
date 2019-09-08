031900873cd8aa4cee723a1e0f424520de166aed
Tomasz Posłuszny <tefposluszny@gmail.com>
2019-09-08 23:43:10 +0200

    Add Spring REST docs to authentication controller

--- src/test/java/com/devpeer/calories/auth/AuthRestControllerTests.java
+++ src/test/java/com/devpeer/calories/auth/AuthRestControllerTests.java
@@ -1,39 +1,44 @@
 package com.devpeer.calories.auth;
 
-import com.devpeer.calories.CaloriesApplication;
-import com.devpeer.calories.auth.AuthRestController;
-import com.devpeer.calories.auth.CustomUserDetailsService;
 import com.devpeer.calories.auth.jwt.JwtTokenProvider;
+import com.devpeer.calories.auth.model.AuthenticationRequest;
+import com.devpeer.calories.core.jackson.Jackson;
+import com.devpeer.calories.user.UserRepository;
 import com.devpeer.calories.user.model.Authority;
 import com.devpeer.calories.user.model.User;
-import com.devpeer.calories.user.UserRepository;
-import com.devpeer.calories.core.jackson.Jackson;
-import com.devpeer.calories.auth.model.AuthenticationRequest;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = AuthRestController.class)
-@ContextConfiguration(classes = {CaloriesApplication.class, JwtTokenProvider.class, CustomUserDetailsService.class})
 public class AuthRestControllerTests {
+
     @Autowired
+    private WebApplicationContext context;
+
     private MockMvc mvc;
 
     @MockBean
@@ -44,6 +49,16 @@ public class AuthRestControllerTests {
 
     @MockBean
     private UserRepository userRepository;
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
+    }
 
     // All users can sign-in
     @Test
@@ -61,7 +76,8 @@ public class AuthRestControllerTests {
         mvc.perform(post("/v1/auth/signin").contentType(MediaType.APPLICATION_JSON).content(Jackson.toJsonUnsafe(authenticationRequest)))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.username", is(user.getUsername())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.token", is("TOKEN")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token", is("TOKEN")))
+        .andDo(document("signin-example"));
 
         AuthenticationRequest unauthorizedRequest = new AuthenticationRequest("user2", "invalid");
 
